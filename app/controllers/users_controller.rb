4eb37b4b92bcd82b0f0626302cff0daae5dc7894
Shunsuke Saruwatari <ssaruwatari@agentgate.jp>
2017-03-04 07:49:45 +0000

    after mentoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
   
   def show
-    @user = User.find(params[:id])
   end
   
   def new
@@ -19,11 +20,9 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       redirect_to @user
     else
@@ -34,6 +33,14 @@ class UsersController < ApplicationController
     private
     
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :location, :password, :password_confirmation)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
+  def correct_user
+    redirect_to root_path if @user != current_user
   end
 end
