bd6690c02584c50e4e9ed4bde3b1ead697a6c42f
Shunsuke Saruwatari <ssaruwatari@agentgate.jp>
2017-03-07 08:27:17 +0000

    Unfollow button

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -7,7 +7,7 @@ class RelationshipsController < ApplicationController
     end
     
     def destroy
-        @user = current_user.following_relationships.find(params[:id]).followed_id
+        @user = current_user.following_relationships.find(params[:id]).followed
         current_user.unfollow(@user)
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :followings, :followers]
   before_action :correct_user, only: [:edit, :update]
   
   def show
@@ -31,7 +31,15 @@ class UsersController < ApplicationController
     end
   end
   
-    private
+  def followings
+    redirect_to @user
+  end
+  
+  def followers
+    redirect_to @user
+  end
+  
+  private
     
   def user_params
     params.require(:user).permit(:name, :email, :location, :password, :password_confirmation)
