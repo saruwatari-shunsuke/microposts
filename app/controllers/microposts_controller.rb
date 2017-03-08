d86f6d9398236683e7cfbf25ff265bfc51dc0e37
Shunsuke Saruwatari <ssaruwatari@agentgate.jp>
2017-03-08 09:51:42 +0000

    add home view's timeline

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -6,6 +6,7 @@ class MicropostsController < ApplicationController
             flash[:success] = "Micropost created!"
             redirect_to root_url
         else
+            
             render 'static_pages/home'
         end
     end
