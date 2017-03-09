f7e6d93731f918580f58a801ce989ecbd79c7f1a
Shunsuke Saruwatari <ssaruwatari@agentgate.jp>
2017-03-09 06:11:05 +0000

    feed paging

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -6,7 +6,6 @@ class MicropostsController < ApplicationController
             flash[:success] = "Micropost created!"
             redirect_to root_url
         else
-            
             render 'static_pages/home'
         end
     end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,9 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @micropost = current_user.microposts.build
-      @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
+      # @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
+      # 10 items
+      @feed_items = current_user.feed_items.includes(:user).page(params[:page]).per(10).order(created_at: :desc)
     end
   end
 end
